6e133baea61707f08e65c1e35b8e744ea5e20cfe
wq0426 <wq0426@163.com>
2024-09-19 13:14:51 +0800

    go simple examples

---
+++ sha256/sha256.go
@@ -0,0 +1,19 @@
+package localsha256
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func Sha256() {
+	s := "sha256 this string"
+
+	h := sha256.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
+}
